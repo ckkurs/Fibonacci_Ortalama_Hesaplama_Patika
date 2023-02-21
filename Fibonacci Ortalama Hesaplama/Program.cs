0006298ef80a96125444f4b65f4ecda2cb975f2e
cemkndz <120122784+ckkurs@users.noreply.github.com>
2023-02-21 17:05:11 +0300

    Fibonacci_Ortalama_Hesaplama_Patika

---
+++ Fibonacci Ortalama Hesaplama/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Fibonacci_Ortalama_Hesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int derinlik;
+
+            Console.WriteLine("Fibonacci Derinliği Giriniz: ");
+            derinlik =int.Parse(Console.ReadLine());
+
+            int[] fib = new int[derinlik];
+            fib[0]=0;
+
+            if(derinlik>1)
+            {
+                fib[1]=1;
+            }
+
+            for (int i = 2; i < derinlik; i++)
+            {
+                fib[i]=fib[i-1] + fib[i-2];
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < derinlik; i++)
+            {
+                sum = sum + fib[i];
+            }
+
+            double ortalama = sum / derinlik;
+            Console.WriteLine("Fibonacci serisi ortalaması: "+ ortalama);
+        }
+    }
+}
